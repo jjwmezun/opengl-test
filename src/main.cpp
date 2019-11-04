1611335ba22cb12c3821cde2e850861c483ff579
jjwmezun <jjwmezun@gmail.com>
2019-11-03 16:04:26 -0800

    Clean up timing code in main

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,12 @@ int main( int argc, char** argv )
         return -1;
     }
 
+    RectGFX square = rect_gfx_create
+    (
+        { 16.0f, 16.0f, 16.0f, 16.0f },
+        { 1.0f, 0.0f, 0.0f, 1.0f }
+    );
+
     double times_list[ TIMER_LIMIT ];
     int times_counter = 0;
     double current_time = glfwGetTime();
@@ -25,6 +31,7 @@ int main( int argc, char** argv )
     {
         current_time = glfwGetTime();
         render_start();
+        render_rect( square );
         render_present();
 
         double render_time = glfwGetTime() - current_time;
@@ -32,33 +39,32 @@ int main( int argc, char** argv )
         ++times_counter;
         if ( times_counter == TIMER_LIMIT )
         {
+            double max = 0;
+            double min = 9999.9;
+            double total = 0;
+            for ( int i = 0; i < TIMER_LIMIT; ++i )
+            {
+                if ( times_list[ i ] > max )
+                {
+                    max = times_list[ i ];
+                }
+                if ( times_list[ i ] < min )
+                {
+                    min = times_list[ i ];
+                }
+                total += times_list[ i ];
+            }
+            double average = total / TIMER_LIMIT;
+
+            printf( "MAX: %f\n", max );
+            printf( "MIN: %f\n", min );
+            printf( "AVG: %f\n", average );
             break;
         }
 
         /* Poll for and process events */
         glfwPollEvents();  
     }
-
-    double max = 0;
-    double min = 9999.9;
-    double total = 0;
-    for ( int i = 0; i < TIMER_LIMIT; ++i )
-    {
-        if ( times_list[ i ] > max )
-        {
-            max = times_list[ i ];
-        }
-        if ( times_list[ i ] < min )
-        {
-            min = times_list[ i ];
-        }
-        total += times_list[ i ];
-    }
-    double average = total / TIMER_LIMIT;
-
-    printf( "MAX: %f\n", max );
-    printf( "MIN: %f\n", min );
-    printf( "AVG: %f\n", average );
 
     game_close();
     return 0;
